903010ed9f5b9659c515ec6e4274961627767aae
lanai-giani <lanaigiani11@gmail.com>
2025-06-23 18:39:47 -0300

    vender

--- php/verifica_venda.php
+++ php/verifica_venda.php
@@ -2,12 +2,12 @@
 session_start();
 
 if (isset($_SESSION['usuario'])) {
-    header("Location: ../vender.html"); 
+    header("Location: ../vender.php"); 
     exit();
 } else {
     echo "<script>
         alert('Você precisa estar logado para vender.');
-        window.location.href = 'login.html';
+        window.location.href = '../login.html';
     </script>";
     exit();
 }
